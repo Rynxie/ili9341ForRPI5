414b6fdbd38010da7c6843368216901b7cf4f565
Rynxie <kayatunahan24@gmail.com>
2025-05-01 21:02:58 +0300

    setting bg color to white

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main() {
 
 	ILI9341_DisplayToggle(ILI9341_DISPLAY_ON);
     ILI9341_Init();
+    ILI9341_FillScreen(ILI9341_WHITE);
     ILI9341_FillRectangle(50,50,50,50,ILI9341_BLUE);
     ILI9341_WriteString(100, 100, "Arda'nin mk knk",Font_7x10, ILI9341_BLACK, ILI9341_WHITE);
 
